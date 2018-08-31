267c6898d56c88421c4ea2687de835a056ef7e64
Julian <brandontabona@gmail.com>
2018-08-31 17:12:44 +0200

    update

--- resources/views/layouts/footer/default-footer.blade.php
+++ resources/views/layouts/footer/default-footer.blade.php
@@ -4,5 +4,6 @@
             © International Data Week 2018. All rights reserved
         </span>
         <br> Designed And Developed by <a href="http://www.optimumqbw.com" target="_blank">Optimum Q</a>
+        <img src="/images/OQ-INFINITE-B-150X84.gif" style=" width: 150px !important; display: block; margin: 0px auto;">
     </p>
 </div>
