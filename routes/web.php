e97cc2236130a402eba26950de16c02a3911cf34
Julian <brandontabona@gmail.com>
2018-10-25 09:10:11 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -595,19 +595,16 @@ Route::get('download', function () {
                 $amount = number_format($FailedTransaction->amount, 2);
                 $payment_type = $FailedTransaction->payment_type;
             } else {
-                $amount = '';
-                $payment_type = '';
-                $package_type = '';
                 $result = 'N/A';
             }
         } else {
-            $amount = '';
-            $payment_type = '';
-            $package_type = '';
             $result = 'N/A';
         }
 
         if ($result != 'PAID') {
+            $amount = '';
+            $payment_type = '';
+            $package_type = '';
             $hasPaid = 'NO';
         } else {
             $hasPaid = 'YES';
